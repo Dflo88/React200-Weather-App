008481ff2c9ba143f8759cabad890e10d8f560df
Diego <dflores7237@gmail.com>
2020-01-13 17:04:11 -0800

    updated SearchBar reducer and added a switch statement

--- src/js/components/SearchBar/SearchBarReducer.js
+++ src/js/components/SearchBar/SearchBarReducer.js
@@ -4,5 +4,29 @@ const defaultState = {
 };
 
 export default function SearchBarReducer (state = defaultState, action){
-    return state;
+    const { type, payload } = action;
+
+    switch (type) {
+        case 'UPDATE_SEARCHBAR_DESCRIPTION': {
+            return {
+                ...state,
+                description: payload.description
+            };
+        }
+
+        case 'SEARCH_CITY': {
+            const { description } = action.payload;
+            return {
+                description: '',
+                lineItems: [
+                    ...state.lineItems,
+                    { description }
+                ]
+            };
+        }
+
+        default: {
+            return state;
+        }
+    }  
 }
